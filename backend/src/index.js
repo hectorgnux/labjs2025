29e55a845a9271369444bd756db6399b427f2a40
hectorgnux <hectorgnux@gmail.com>
2025-03-20 17:25:58 -0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
+
+// Habilitar CORS para el origen del frontend
+app.use(cors({
+    origin: 'http://localhost:81' // o 'http://127.0.0.1:81', dependiendo de cómo accedas
+}));
 
 app.get('/time', (req, res) => {
     const now = new Date().toLocaleString('es-CL', { timeZone: 'America/Santiago' });
